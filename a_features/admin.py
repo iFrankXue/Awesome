c5286b0407fbc5f930b000747c57fbe6dec557fc
Frank Zhiming Xue <ifrankxue@gmail.com>
2024-11-06 22:09:59 -0800

    Feature toggle and herobutton

---
+++ a_features/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Feature
+
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'id',
+        'developer',
+        'staging_enabled',
+        'production_enabled',
+    )
+
+admin.site.register(Feature, FeatureAdmin)
