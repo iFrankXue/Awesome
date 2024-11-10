2c2432a24933532a206336e3e16bc57b59958ec6
Frank Zhiming Xue <ifrankxue@gmail.com>
2024-11-10 12:20:50 -0800

    SEO settings

--- a_posts/models.py
+++ a_posts/models.py
@@ -22,6 +22,9 @@ class Post(models.Model):
     class Meta:
         ordering = ['-created']
         
+    def get_absolute_url(self):
+        return f'/post/{self.id}/'
+        
         
 class LikedPost(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -45,6 +48,8 @@ class Tag(models.Model):
     class Meta:
         ordering = ['order']
         
+    def get_absolute_url(self):
+        return f'/category/{self.slug}/'
         
         
 class Comment(models.Model):
